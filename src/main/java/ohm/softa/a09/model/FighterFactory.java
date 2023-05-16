1d64176a83a3c30fb3a3cc29fe0d7c92114f8a0f
joshuabrandes <47532061+joshuabrandes@users.noreply.github.com>
2023-05-16 10:43:47 +0200

    I'm human

--- src/main/java/ohm/softa/a09/model/FighterFactory.java
+++ src/main/java/ohm/softa/a09/model/FighterFactory.java
@@ -6,10 +6,13 @@ import ohm.softa.a09.model.empire.TieInterceptor;
 import ohm.softa.a09.model.rebellion.AWing;
 import ohm.softa.a09.model.rebellion.XWing;
 import ohm.softa.a09.model.rebellion.YWing;
+import ohm.softa.a09.resource.FxImageLoaderAdapter;
 import ohm.softa.a09.resource.ResourceLoader;
 import ohm.softa.a09.util.NameGenerator;
 import javafx.scene.image.Image;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 /**
@@ -23,34 +26,48 @@ public final class FighterFactory {
 	private static final int NumberOfKnownFighterTypes = 6;
 	private final Random random;
 	private final NameGenerator nameGenerator;
-	private final ResourceLoader<Image> imageResourceLoader;
+	private final FxImageLoaderAdapter imageLoader;
 
 	public FighterFactory() {
 		nameGenerator = new NameGenerator();
 		random = new Random();
-		imageResourceLoader = new ResourceLoader<>(Image::new);
+		imageLoader = new FxImageLoaderAdapter();
 	}
 
 	/**
 	 * Create a random Fighter
 	 *
-	 * @implNote the method has an implementation flaw because it always loads the fighters image
 	 * @return a new Fighter instance
+	 * @implNote the method has an implementation flaw because it always loads the fighters image
 	 */
 	public Fighter createFighter() {
+		var name = nameGenerator.generateName();
+
 		switch (random.nextInt(NumberOfKnownFighterTypes)) {
 			case 0:
-				return new AWing(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/awing.jpg"));
+				return new AWing(name, getImage("fighter/awing.jpg"));
 			case 1:
-				return new XWing(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/xwing.jpg"));
+				return new XWing(name, getImage("fighter/xwing.jpg"));
 			case 2:
-				return new YWing(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/ywing.jpg"));
+				return new YWing(name, getImage("fighter/ywing.jpg"));
 			case 3:
-				return new TieBomber(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/tiebomber.jpg"));
+				return new TieBomber(name, getImage("fighter/tiebomber.jpg"));
 			case 4:
-				return new TieFighter(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/tiefighter.jpg"));
+				return new TieFighter(name, getImage("fighter/tiefighter.jpg"));
 			default:
-				return new TieInterceptor(nameGenerator.generateName(), imageResourceLoader.loadResource(ClassLoader.getSystemClassLoader(), "fighter/tieinterceptor.jpg"));
+				return new TieInterceptor(name, getImage("fighter/tieinterceptor.jpg"));
 		}
+	}
+
+	// Flyweight pattern
+	private Map<String, Image> cache = new HashMap<>();
+	private final ClassLoader classLoader = ClassLoader.getSystemClassLoader();
+
+	private Image getImage(String path) {
+		// adds image if it isn't already in the cache
+		cache.computeIfAbsent(path,
+			image -> imageLoader.loadImage(path));
+
+		return cache.get(path);
 	}
 }
